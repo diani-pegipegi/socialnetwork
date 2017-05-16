fa2e8ae02a056e01827d9a9efaa769d85702e896
Diani Pavitri Rahasta <dianipavitrirahasta@as-MacBook-Pro.local>
2017-05-16 15:10:45 +0700

    Completed sign in UI custom classes

---
+++ socialnetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  socialnetwork
+//
+//  Created by Diani Pavitri Rahasta on 5/16/17.
+//  Copyright © 2017 Diani Pavitri Rahasta. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 128.0 / 255.0

6146216b835de5fcdb8d2f8d7ae7369a86b3f176
Diani Pavitri Rahasta <dianipavitrirahasta@as-MacBook-Pro.local>
2017-05-17 09:56:27 +0700

    Completed Facebook Authentication and Firebase auth connection

--- socialnetwork/SignInVC.swift
+++ socialnetwork/SignInVC.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var facebookLoginButton: RoundButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,32 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("JESS: User cancelled Facebook authentication")
+            } else {
+                print("JESS: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: {(user, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("JESS: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 

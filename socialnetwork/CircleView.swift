da7536633f6b2d3ad17cf82ab3d58116d10a1808
Diani Pavitri Rahasta <dianipavitrirahasta@as-MacBook-Pro.local>
2017-05-17 15:01:43 +0700

    Completed feed UI part 1

--- socialnetwork/CircleView.swift
+++ socialnetwork/CircleView.swift
@@ -1,14 +1,14 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  socialnetwork
 //
-//  Created by Diani Pavitri Rahasta on 5/16/17.
+//  Created by Diani Pavitri Rahasta on 5/17/17.
 //  Copyright © 2017 Diani Pavitri Rahasta. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,7 +17,13 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        
     }
-
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        layer.cornerRadius = self.frame.width / 2
+        
+    }
 }
